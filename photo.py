92a855efdb749e369bb52839894d028e0e4d22d8
Johnny <johnnyhsieh@inquartik.com>
2021-01-01 23:11:06 +0800

    add mom and dad

--- photo.py
+++ photo.py
@@ -4,5 +4,7 @@ photo_dict = {
     '謝中耘' :'https://www.micelle0926.com/file/www.micelle0926/f14/n13260/0.jpg',
     '陳虹君' :'https://scontent.ftpe8-4.fna.fbcdn.net/v/t1.0-9/12985489_1604866596505394_6519126741118046324_n.jpg?_nc_cat=110&ccb=2&_nc_sid=f9d7a1&_nc_ohc=YwPP-tdpt2IAX8rewoA&_nc_ht=scontent.ftpe8-4.fna&oh=78f98e655f82e46ab9035b64782d3b14&oe=5FFFEF52',
     '戴寓婕' :'https://instagram.ftpe7-2.fna.fbcdn.net/v/t51.2885-15/e35/16229171_676059919234785_5464427529957801984_n.jpg?_nc_ht=instagram.ftpe7-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=JI79KCoHjtIAX92mJdc&tp=1&oh=72e1b41bd3d02fa15ad50710ead68dbd&oe=600552A9',
-    '十銓劍' :'https://i1.kknews.cc/SIG=3n57shc/ctp-vzntr/q33rq4q6s7664250o842n38n0p5rr50s.jpg'
+    '十銓劍' :'https://i1.kknews.cc/SIG=3n57shc/ctp-vzntr/q33rq4q6s7664250o842n38n0p5rr50s.jpg',
+    '媽仔' :'https://scontent.ftpe8-2.fna.fbcdn.net/v/t31.0-8/19983403_1596747280344454_1037271695859462145_o.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=i3rtkaeZDuAAX-q62-u&_nc_ht=scontent.ftpe8-2.fna&oh=5bafd5be529415e790cba3ad6b5f181d&oe=60140C73',
+    '爸仔' :'https://scontent.ftpe8-2.fna.fbcdn.net/v/t1.0-9/1506654_666049786791371_602766181_n.jpg?_nc_cat=101&ccb=2&_nc_sid=cdbe9c&_nc_ohc=0WekjUBG_pkAX-cdmGj&_nc_ht=scontent.ftpe8-2.fna&oh=3be09e21777443d797ac5ed094a038cf&oe=6013053A'
 }
